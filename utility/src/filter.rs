b5b4edab2153a3aa3572f7410ed6ae9487dec8b5
yguenduez <yasinguenduez1@gmail.com>
2024-12-25 11:20:48 +0100

    add monorepo - as util should be platform agnostic

---
+++ utility/src/filter.rs
@@ -0,0 +1,15 @@
+struct Filter;
+
+impl Filter {
+    pub fn get_new_median(new_values: &[i16])->i16{
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests{
+    #[test]
+    fn test_get_new_median(){
+        assert_eq!(2, 3);
+    }
+}
---
+++ utility/src/lib.rs
@@ -0,0 +1,5 @@
+mod filter;
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
