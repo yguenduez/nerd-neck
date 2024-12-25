1f386de4a48b98e3d5b0a66ea2833cdb729bc747
yguenduez <yasinguenduez1@gmail.com>
2024-12-25 19:48:58 +0100

    add embassy

--- nerd-neck/src/main.rs
+++ nerd-neck/src/main.rs
@@ -1,15 +1,27 @@
 #![no_std]
 #![no_main]
 
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
 use bmi160::{AccelerometerPowerMode, Bmi160, GyroscopePowerMode, SensorSelector, SlaveAddr};
 use esp_backtrace as _;
 use esp_hal::i2c::master::{Config, I2c};
 use esp_hal::prelude::*;
 use esp_println::println;
 
-#[entry]
-fn main() -> ! {
-    let peripherals = esp_hal::init(esp_hal::Config::default());
+#[main]
+async fn main(_spawner: Spawner) -> ! {
+    let peripherals = esp_hal::init({
+        let mut config = esp_hal::Config::default();
+        config.cpu_clock = CpuClock::max();
+        config
+    });
+
+    esp_println::logger::init_logger_from_env();
+
+    let timer0 = esp_hal::timer::systimer::SystemTimer::new(peripherals.SYSTIMER)
+        .split::<esp_hal::timer::systimer::Target>();
+    esp_hal_embassy::init(timer0.alarm0);
 
     // Getting the i2c up
     let i2c = I2c::new(peripherals.I2C0, Config::default())
